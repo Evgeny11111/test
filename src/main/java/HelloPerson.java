d71bb8904a4d88de484d25e54b920b3d7e8ac927
Evgeny11111 <81196524+Evgeny11111@users.noreply.github.com>
2021-12-23 10:48:10 +0300

    add HelloPerson changes

--- src/main/java/HelloPerson.java
+++ src/main/java/HelloPerson.java
@@ -1,10 +1,2 @@
 public class HelloPerson {
-    private String name;
-    public HelloPerson(String name) {
-        this.name = name;
-    }
-
-    public String say(){
-        return "Hello " + name;
-    }
 }
